331951a139e57050a6daedb4058285693003426b
ameeenamzz <shoukethameen@gmail.com>
2025-01-11 09:28:34 +0530

    add basic GUI setup for Calculator class

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Calculator {
+
+    public Calculator(){
+        JFrame jf=new JFrame();
+        jf.setLayout(null);
+        jf.setSize(600,600);
+
+        jf.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Calculator();
+    }
+}
